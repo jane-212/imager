c766d1bbe29631b762b59428d8f8c8eae71510bc
jane <jjt1525@outlook.com>
2022-10-22 21:58:37 +0800

    add image

--- imager-backend/source.sql
+++ imager-backend/source.sql
@@ -3,4 +3,6 @@ create table image(
     id bigint(20) unsigned not null auto_increment,
     url varchar(255) not null,
     primary key(id)
-)
+);
+insert into image(url) values(`https://pictu1.1plq.com/p1587/2021/02/21/p1587-5434-72063.jpg`);
+insert into image(url) values(`https://pictu1.1plq.com/p1587/2021/02/21/p1587-5434-72064.jpg`);
